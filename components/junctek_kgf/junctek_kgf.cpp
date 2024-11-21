7acd74e2b26e7142b318d6eb0f801b705e1abb1d
alanv72 <alan@brosinchrist.com>
2024-11-20 19:12:11 -0600

    Update junctek_kgf.cpp

--- components/junctek_kgf/junctek_kgf.cpp
+++ components/junctek_kgf/junctek_kgf.cpp
@@ -144,7 +144,7 @@ void JuncTekKGF::handle_status(const char* buffer)
   const float runtimeSeconds = getval(cursor);
   const float temperature = getval(cursor) - 100.0;
   const float powerInWatts = getval(cursor);
-  const int functionPending = getval(cursor)
+  const int functionPending = getval(cursor);
   const int relayStatus = getval(cursor);
   const int direction = getval(cursor);
   const int batteryLifeMinutes = getval(cursor);
